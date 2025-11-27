ca6fce64d2a97abb2c05ff389e65e84248635202
m073med011 <166988534+m073med011@users.noreply.github.com>
2025-11-27 20:21:24 +0200

    chore: remove a deleted file.

--- dist/src/modules/database/database.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Connection, ClientSession } from 'mongoose';
-export declare class DatabaseService {
-    private readonly connection;
-    constructor(connection: Connection);
-    withTransaction<T>(operation: (session: ClientSession) => Promise<T>): Promise<T>;
-}
--- dist/src/modules/otp/otp.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { OtpService } from './otp.service';
-export declare class OtpController {
-    private readonly otpService;
-    constructor(otpService: OtpService);
-    generate(generateOtpDto: any): Promise<import("./schemas/otp.schema").OtpDocument>;
-    verify(verifyOtpDto: any): {
-        status: string;
-    };
-}
