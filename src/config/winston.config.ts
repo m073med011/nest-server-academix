e102b9421feb1a69869dc2555ee27d15c8784713
m073med011 <166988534+m073med011@users.noreply.github.com>
2025-11-27 20:55:39 +0200

    feat: add Winston logger configuration for console and file transports

--- src/config/winston.config.ts
+++ src/config/winston.config.ts
@@ -13,20 +13,24 @@ export const winstonConfig = {
         }),
       ),
     }),
-    new winston.transports.File({
-      filename: 'logs/error.log',
-      level: 'error',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
-    }),
-    new winston.transports.File({
-      filename: 'logs/combined.log',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
-    }),
+    ...(process.env.NODE_ENV !== 'production'
+      ? [
+          new winston.transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+            format: winston.format.combine(
+              winston.format.timestamp(),
+              winston.format.json(),
+            ),
+          }),
+          new winston.transports.File({
+            filename: 'logs/combined.log',
+            format: winston.format.combine(
+              winston.format.timestamp(),
+              winston.format.json(),
+            ),
+          }),
+        ]
+      : []),
   ],
 };
