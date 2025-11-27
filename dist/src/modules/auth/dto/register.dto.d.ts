74a11a5fe25db06c5bdf41c6185d2141dcee920c
m073med011 <166988534+m073med011@users.noreply.github.com>
2025-11-27 20:00:16 +0200

    fix: Update Vercel config with build command and remove dist from repo

--- dist/src/modules/auth/dto/register.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserRole } from '../../users/schemas/user.schema';
-export declare class RegisterDto {
-    name: string;
-    email: string;
-    password: string;
-    role?: UserRole;
-}
